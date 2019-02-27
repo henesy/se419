f9a806685fc86d86836a1c0177cde583eb573261
henesy <henesy.dev@gmail.com>
2019-02-27 16:38:34 -0600

    fix some bugs

--- lab3/ExperimentOne.java
+++ lab3/ExperimentOne.java
@@ -33,8 +33,8 @@ public class ExperimentOne {
 
 		// Change following paths accordingly
 		String input = "/cpre419/patents.txt"; 
-		String temp = "/user/nlosby/lab3/exp1/temp";
-		String output = "/user/nlosby/lab3/exp1/output/"; 
+		String temp = "/user/seh/lab3/exp1/temp";
+		String output = "/user/seh/lab3/exp1/output/"; 
 
 		// The number of reduce tasks 
 		int reduce_tasks = 10; 

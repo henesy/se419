2f43db61fad5a70a40200b04b250ba8091e1fe7c
henesy <henesy.dev@gmail.com>
2019-03-05 20:23:48 -0600

    quick order check

--- workspace/lab4/src/exp2/Lab4Exp2.java
+++ workspace/lab4/src/exp2/Lab4Exp2.java
@@ -8,10 +8,41 @@ package exp2;
 
 Actual data set:
 
-
+[seh@hadoop000 src]$ hdfs dfs -cat /cpre419/input-50m | tail
+SLF4J: Class path contains multiple SLF4J bindings.
+SLF4J: Found binding in [jar:file:/opt/hadoop/hadoop/share/hadoop/common/lib/slf4j-log4j12-1.7.25.jar!/org/slf4j/impl/StaticLoggerBinder.class]
+SLF4J: Found binding in [jar:file:/opt/hadoop/tez/lib/slf4j-log4j12-1.7.10.jar!/org/slf4j/impl/StaticLoggerBinder.class]
+SLF4J: See http://www.slf4j.org/codes.html#multiple_bindings for an explanation.
+SLF4J: Actual binding is of type [org.slf4j.impl.Log4jLoggerFactory]
+Kejc2HviJQcagVh IEdVRI1v63gjJtWDlC3RJjrSPgtzRJARNV235Uqi
+Wa0gnyFIQr4gmiW NbL3o3R2m1EkuF5rryc73ijBKPqNz4R6rUUIe0oH
+nSKmaPWyNTuRnkb qcu5Sg3BIlibOysLbTdGCREBiCKQtdCr6bHGKbrj
+jIUq5DOASJpZndL dgc4ezXV4mU22pCaMo5ax8NBBCereYZtVtC1NDGi
+zDDiqKBaKPgmz3M gEEiu0StXTOGPRTHMb6E1TJHKautN2ur43ZyHkgl
+MMUjesqpEs7KoCd FeNhIgqOIkcKJ5iRCOi91ZZtkFPPEnJKLXNcoJ0s
+KUgYzUPQM5a1FI5 ZlFuVY7fIXjWNlcPQlUpCB4R8tdcAvVOrnvQThun
+JGeIQQ327GCZKqV EZ7TIlto3234Hfn4QmBOSk4isubPd1fgnIUA5fFE
+eQTA3gWaMTKdfxL rkFLaWtqz5D7HP09N9zyi6JjiyxYTFr4hksaYHtC
+tCquLsEZGeekjl7 HJ8OrSizMdVUL64XND6F827gv4Rl6D6WjoueCv7F
 
 Our output (Ran in 1m, 8s):
 
+[seh@hadoop000 src]$ hdfs dfs -cat /user/seh/lab4/exp2/temp/part-r-00009 | tail
+SLF4J: Class path contains multiple SLF4J bindings.
+SLF4J: Found binding in [jar:file:/opt/hadoop/hadoop/share/hadoop/common/lib/slf4j-log4j12-1.7.25.jar!/org/slf4j/impl/StaticLoggerBinder.class]
+SLF4J: Found binding in [jar:file:/opt/hadoop/tez/lib/slf4j-log4j12-1.7.10.jar!/org/slf4j/impl/StaticLoggerBinder.class]
+SLF4J: See http://www.slf4j.org/codes.html#multiple_bindings for an explanation.
+SLF4J: Actual binding is of type [org.slf4j.impl.Log4jLoggerFactory]
+zzzELTQ3vylDUmR a8fHoXzae5M3QGyaiujjdsMC45faVUrq1oA9ktOe
+zzzMC6e3LaIXxOS L5MOLs3yF52Zx5P78yKJBHzBBrACx6Fkj1ek2TZj
+zzzRhNL67R3TOpk e3hBNt9My9artGWV2HcXE26eoWEeayY9I1tpkC5Z
+zzzT2AxjUKKo5uY rJPMt3SY0yyPLknsjt79FUFWS9fjHqafU6AsvO2r
+zzziapoVW6xoE4h 6QZpMCxhezizO18JNf4A7H1aBpm3QZ8UrqBNUKC9
+zzzjmbBqj0zjyTS 1vkoRF2XJAxseaWvhxTPj5hY4NcsT7sAAgE45gIC
+zzzoGlnWjyVM4PK sK1nOKL1cIgeA2SPNJdfRmPSbuuBW0ToCpGJvsxA
+zzzpG2TH8f4OOG8 NIYN0i0X3SLsjEeDoGN7WbXmpDk9uQcZHnK6CZIj
+zzzrS7l9I3x7JTT ZNHg28osyR8UALOEk1l6EBTxdAO19CUoxfMfDQnF
+zzzzJXxBguTEdpM eWMIDSpNlWHUWXuttlFkbCV84pRprMucRjpmyyQ7
 
 
  */
@@ -90,10 +121,12 @@ public class Lab4Exp2 {
 		FileInputFormat.addInputPath(job_one, new Path(input));
 		FileOutputFormat.setOutputPath(job_one, new Path(temp));
 		
+		job_one.setPartitionerClass(MyPartitioner.class);
+
+		
 		job_one.waitForCompletion(true);
 		
 		// TOP for this job
-		job_one.setPartitionerClass(MyPartitioner.class);
 
 	}
 
